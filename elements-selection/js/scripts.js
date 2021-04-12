c1c1b1fbbdaae52cd4cd352ab381fc14f4369ba2
Luis Fernando M <luisfernandomgrs@gmail.com>
2021-04-12 08:43:15 -0300

    working with Get Elements...

---
+++ elements-selection/js/scripts.js
@@ -0,0 +1,12 @@
+
+
+// qual usar ?
+// getElementById (element)
+// getElementByClassName (HTMLColection)
+// getElementByTagName (HTMLCollection)
+// querySelector (element)
+// querySelectorAll (Nodelist)
+
+const elements = document.querySelectorAll(".one");
+
+elements.forEach(el => console.log(el));
